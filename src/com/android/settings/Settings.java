49f862cd87c9782852f8a628aafe564b9fbb688e
maxwen <maxwen@users.noreply.github.com>
2021-10-24 07:59:56 +0000

    Settings: Build OctaviLab into Settings
    Signed-off-by: Sagarrokade006 <sagarrokade006@gmail.com>
    Signed-off-by: Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -436,4 +436,7 @@ public class Settings extends SettingsActivity {
      * Activity for OneHandedSettings
      */
     public static class OneHandedSettingsActivity extends SettingsActivity { /* empty */ }
+
+    // OctaviLab Additoon
+    public static class OctaviLabActivity extends SettingsActivity {}
 }
