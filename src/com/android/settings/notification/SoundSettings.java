31157ee426a2ca05e08190ebbe7ee423942a5ce0
cjybyk <cjybyjk@zjnu.edu.cn>
2022-02-05 13:32:25 +0000

    Settings: support per-app volume [3/3]
    Change-Id: I3e72a4b0fda68a5cd1e9a35fc161a1d7ddae6d7e
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>

--- src/com/android/settings/notification/SoundSettings.java
+++ src/com/android/settings/notification/SoundSettings.java
@@ -303,6 +303,8 @@ public class SoundSettings extends DashboardFragment implements OnActivityResult
                 new VibrateIconPreferenceController(context, fragment, lifecycle);
         final ScreenshotSoundPreferenceController screenshotSoundPreferenceController =
                 new ScreenshotSoundPreferenceController(context, fragment, lifecycle);
+        final AppVolumePreferenceController appVolumePreferenceController =
+                new AppVolumePreferenceController(context, fragment, lifecycle);
 
         controllers.add(dialPadTonePreferenceController);
         controllers.add(screenLockSoundPreferenceController);
@@ -314,6 +316,7 @@ public class SoundSettings extends DashboardFragment implements OnActivityResult
         controllers.add(dockAudioMediaPreferenceController);
         controllers.add(bootSoundPreferenceController);
         controllers.add(emergencyTonePreferenceController);
+        controllers.add(appVolumePreferenceController);
         controllers.add(new PreferenceCategoryController(context,
                 "other_sounds_and_vibrations_category").setChildren(
                 Arrays.asList(dialPadTonePreferenceController,
