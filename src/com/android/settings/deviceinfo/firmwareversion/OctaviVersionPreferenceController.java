d3e3b6e097b3efc1fea669701d7faecc1c8f69d4
Erik Drozina <erikdro41@gmail.com>
2022-09-19 21:50:12 +0200

    Settings: OctaviVersion: Fix Octavi version prop and remove the easter egg from the tile
    It's pointless to have 2 easter egg trigger in the same page. Let's continue to use only the Android version one
    
    Signed-off-by: Erik Drozina <erikdro41@gmail.com>

--- src/com/android/settings/deviceinfo/firmwareversion/OctaviVersionPreferenceController.java
+++ src/com/android/settings/deviceinfo/firmwareversion/OctaviVersionPreferenceController.java
@@ -39,21 +39,11 @@ import com.android.settingslib.RestrictedLockUtilsInternal;
 public class OctaviVersionPreferenceController extends BasePreferenceController {
 
     private static final String TAG = "octaviVersionDialogCtrl";
-    private static final int DELAY_TIMER_MILLIS = 500;
-    private static final int ACTIVITY_TRIGGER_COUNT = 3;
 
-    private static final String KEY_OCTAVI_VERSION_PROP = "ro.octavi.build.version";
-
-    private final UserManager mUserManager;
-    private final long[] mHits = new long[ACTIVITY_TRIGGER_COUNT];
-
-    private RestrictedLockUtils.EnforcedAdmin mFunDisallowedAdmin;
-    private boolean mFunDisallowedBySystem;
+    private static final String KEY_OCTAVI_VERSION_PROP = "ro.octavi.branding.version";
 
     public OctaviVersionPreferenceController(Context context, String key) {
         super(context, key);
-        mUserManager = (UserManager) mContext.getSystemService(Context.USER_SERVICE);
-        initializeAdminPermissions();
     }
 
     @Override
@@ -75,53 +65,5 @@ public class OctaviVersionPreferenceController extends BasePreferenceController 
     public CharSequence getSummary() {
         return SystemProperties.get(KEY_OCTAVI_VERSION_PROP,
                 mContext.getString(R.string.unknown));
-    }
-
-    @Override
-    public boolean handlePreferenceTreeClick(Preference preference) {
-        if (!TextUtils.equals(preference.getKey(), getPreferenceKey())) {
-            return false;
-        }
-        if (Utils.isMonkeyRunning()) {
-            return false;
-        }
-        arrayCopy();
-        mHits[mHits.length - 1] = SystemClock.uptimeMillis();
-        if (mHits[0] >= (SystemClock.uptimeMillis() - DELAY_TIMER_MILLIS)) {
-            if (mUserManager.hasUserRestriction(UserManager.DISALLOW_FUN)) {
-                if (mFunDisallowedAdmin != null && !mFunDisallowedBySystem) {
-                    RestrictedLockUtils.sendShowAdminSupportDetailsIntent(mContext,
-                            mFunDisallowedAdmin);
-                }
-                Log.d(TAG, "Sorry, no fun for you!");
-                return true;
-            }
-
-            final Intent intent = new Intent(Intent.ACTION_MAIN)
-                    .setClassName(
-                            "android", com.android.internal.app.PlatLogoActivity.class.getName());
-            try {
-                mContext.startActivity(intent);
-            } catch (Exception e) {
-                Log.e(TAG, "Unable to start activity " + intent.toString());
-            }
-        }
-        return true;
-    }
-
-    /**
-     * Copies the array onto itself to remove the oldest hit.
-     */
-    @VisibleForTesting
-    void arrayCopy() {
-        System.arraycopy(mHits, 1, mHits, 0, mHits.length - 1);
-    }
-
-    @VisibleForTesting
-    void initializeAdminPermissions() {
-        mFunDisallowedAdmin = RestrictedLockUtilsInternal.checkIfRestrictionEnforced(
-                mContext, UserManager.DISALLOW_FUN, UserHandle.myUserId());
-        mFunDisallowedBySystem = RestrictedLockUtilsInternal.hasBaseUserRestriction(
-                mContext, UserManager.DISALLOW_FUN, UserHandle.myUserId());
     }
 }
